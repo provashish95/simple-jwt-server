e8d0d8e0c8770cb606be7eaafb871ff50a8d778e
Provashish Roy <provashishroy95@gmail.com>
2022-05-12 00:13:50 +0600

    created login api and install jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 
@@ -10,6 +11,13 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+
+app.post('/login', async (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.send({ success: true });
 })
 
 app.listen(port, () => {
